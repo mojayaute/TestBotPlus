f8da616ec5b10f3c661269abce8d23fd1ca3b2d7
David Morales <Davidm@plusquan.com>
2017-10-30 20:11:26 +0000

    new chages

---
+++ Dialogs/QnAMakerResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Dialogs
+{
+    public class QnAMakerResult
+    {
+        /// <summary>
+        /// The top answer found in the QnA Service.
+        /// </summary>
+        [JsonProperty(PropertyName = "answer")]
+        public string Answer { get; set; }
+
+        /// <summary>
+        /// The score in range [0, 100] corresponding to the top answer found in the QnA    Service.
+        /// </summary>
+        [JsonProperty(PropertyName = "score")]
+        public double Score { get; set; }
+    }
+    //De-serialize the response
+    
+    
+}
---
+++ Dialogs/RootDialog.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+
+
+            var activity = await result as Activity;
+
+            string responseString = string.Empty;
+
+            var query = activity.Text; //User Query
+            var knowledgebaseId = "05af3bb1-714b-448c-8fef-be7d7ee22b6d"; // Use knowledge base id created.
+            var qnamakerSubscriptionKey = "9c24f9a92f8d4f6495617833c08fa1c9"; //Use subscription key assigned to you.
+
+            //Build the URI
+            Uri qnamakerUriBase = new Uri("https://westus.api.cognitive.microsoft.com/qnamaker/v1.0");
+            var builder = new UriBuilder($"{qnamakerUriBase}/knowledgebases/{knowledgebaseId}/generateAnswer");
+
+            //Add the question as part of the body
+            var postBody = $"{{\"question\": \"{query}\"}}";
+
+            //Send the POST request
+            using (WebClient client = new WebClient())
+            {
+                //Set the encoding to UTF8
+                client.Encoding = System.Text.Encoding.UTF8;
+
+                //Add the subscription key header
+                client.Headers.Add("Ocp-Apim-Subscription-Key", qnamakerSubscriptionKey);
+                client.Headers.Add("Content-Type", "application/json");
+                responseString = client.UploadString(builder.Uri, postBody);
+            }
+
+
+            QnAMakerResult response;
+            try
+            {
+                response = JsonConvert.DeserializeObject<QnAMakerResult>(responseString);
+            }
+            catch
+            {
+                throw new Exception("Unable to deserialize QnA Maker response string.");
+            }
+
+
+
+
+
+
+
+
+
+            // return our reply to the user
+            await context.PostAsync(response.Answer);
+
+            context.Wait(MessageReceivedAsync);
+        }
+
+
+        
+       
+
+
+        
+
+    }
+}
